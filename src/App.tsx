3effb8a217f784f07155874077a7990e633e4686
tonycao2111 <tonybro2111@gmail.com>
2025-03-19 17:28:43 +0700

    intial project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl underline text-red-100 font-light">
+        Hello world!
+      </h1>
+    </>
+  );
+}
+
+export default App;
